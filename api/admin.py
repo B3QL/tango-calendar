22b4efe333176b27c20e2b5055f217fdd0b6275d
Bartłomiej Kurzeja <bartlomiej+gitlab@kurzeja.it>
2020-11-19 18:13:40 +0100

    Extend default User model with company_id and timezone

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from api.models import APIUser
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Admin model for User class"""
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('company_id', 'timezone')}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('company_id', 'timezone')}),
+    )
+
+
+admin.site.register(APIUser, UserAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
+import uuid
+from pytz import all_timezones
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class APIUser(AbstractUser):
+    """Custom user class"""
+    ALL_TIMEZONES = ((name, name) for name in all_timezones)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'))
+    company_id = models.UUIDField(default=uuid.uuid4)
+    timezone = models.TextField(choices=ALL_TIMEZONES, default=settings.TIME_ZONE)
